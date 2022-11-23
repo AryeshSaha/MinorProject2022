c2c947411060027dbaa0ebc4d22fdf5fb09ca64b
AryeshSaha <aryeshsaha5302@gmail.com>
2022-11-23 19:04:58 +0530

    Validation in signup page

--- signup.php
+++ signup.php
@@ -13,7 +13,7 @@
 
                 <h3>Sign Up</h3>
 
-                <p>You have to fill out this form</p>
+                <p>Fill out this Form</p>
 
             </div>
 
@@ -31,7 +31,7 @@
                     <option value="none" selected>Gender</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
-                    <option value="other">other</option>
+                    <option value="intersex">Intersex</option>
                 </select>
 
                 <input type="number" placeholder="mobile" name="mobile">
@@ -51,44 +51,72 @@
                 // REGISTER USER
                 if (isset($_POST['signup'])) {
                     // receive all input values from the form
+                    $name = mysqli_real_escape_string($con, $_POST['name']);
+                    $fh_name = mysqli_real_escape_string($con, $_POST['fh_name']);
+                    $dob = mysqli_real_escape_string($con, $_POST['dob']);
+                    $mobile = mysqli_real_escape_string($con, $_POST['mobile']);
                     $email = mysqli_real_escape_string($con, $_POST['email']);
                     $password = mysqli_real_escape_string($con, $_POST['pass']);
 
 
+                    //VALIDATION
+                    if ($name == "" && $fh_name == "" && $dob == "" && $mobile == "" && $email == "" && $password == "") {
 
-                    // first check the database to make sure
-                    // a user does not already exist with the same username and/or email
-                    $user_check_query = "SELECT * FROM login WHERE email='$email'  LIMIT 1";
-                    $result = mysqli_query($con, $user_check_query);
-                    $user = mysqli_fetch_assoc($result);
+                        echo "<script>alert('Fields Can not be empty')</script>";
+                    } elseif (strlen($name) < 6 && !preg_match("/^[a-zA-Z-' ]*$/", $name)) {
 
+                        echo "<script>alert('Only letters and white space allowed in Name fields')</script>";
+                    } elseif (strlen($fh_name) < 6 && !preg_match("/^[a-zA-Z-' ]*$/", $fh_name)) {
 
-                    if ($user) { // if user exists
-                        echo "Email already exist";
+                        echo "<script>alert('Only letters and white space allowed in Name fields')</script>";
+                    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+                        echo "<script>alert('Invalid email format')</script>";
+                    } elseif (strlen($mobile) != 10) {
+
+                        echo "<script>alert('Mobile Number must be 10 digits')</script>";
+                    } elseif (strlen($password) < 8) {
+
+                        echo "<script>alert('Password must be at least 8 digits')</script>";
                     } else {
 
+                        // first check the database to make sure
+                        // a user does not already exist with the same username and/or email
+                        $user_check_query = "SELECT * FROM login WHERE email='$email'  LIMIT 1";
+                        $result = mysqli_query($con, $user_check_query);
+                        $user = mysqli_fetch_assoc($result);
 
 
-                        $query1 = "INSERT INTO login (email, pass)
-                          VALUES('$email', '$password')";
-                        mysqli_query($con, $query1);
+                        if ($user) { // if user exists
+                            echo "Email already exist";
+                        } else {
 
-                        $name = mysqli_real_escape_string($con, $_POST['name']);
-                        $dob = mysqli_real_escape_string($con, $_POST['dob']);
-                        $gender = mysqli_real_escape_string($con, $_POST['gender']);
-                        $fh_name = mysqli_real_escape_string($con, $_POST['fh_name']);
-                        $mobile = mysqli_real_escape_string($con, $_POST['mobile']);
 
-                        // Finally, register user if there are no errors in the for
-                        $query = "INSERT INTO details (email, naam, fh_naam,dob,sex,phn)
-                          VALUES('$email','$name' , '$fh_name','$dob','$gender','$mobile')";
-                        mysqli_query($con, $query);
-                        $_SESSION['name'] = $name;
-                        $_SESSION['email'] = $email;
-                        $_SESSION['success'] = "You are now logged in";
-                        header('location: profileEdit.php');
+
+                            $query1 = "INSERT INTO login (email, pass)
+                              VALUES('$email', '$password')";
+                            mysqli_query($con, $query1);
+
+                            $name = mysqli_real_escape_string($con, $_POST['name']);
+                            $dob = mysqli_real_escape_string($con, $_POST['dob']);
+                            $gender = mysqli_real_escape_string($con, $_POST['gender']);
+                            $fh_name = mysqli_real_escape_string($con, $_POST['fh_name']);
+                            $mobile = mysqli_real_escape_string($con, $_POST['mobile']);
+
+                            // Finally, register user if there are no errors in the for
+                            $query = "INSERT INTO details (email, naam, fh_naam,dob,sex,phn)
+                              VALUES('$email','$name' , '$fh_name','$dob','$gender','$mobile')";
+                            mysqli_query($con, $query);
+                            $_SESSION['name'] = $name;
+                            $_SESSION['email'] = $email;
+                            $_SESSION['success'] = "You are now logged in";
+                            header('location: profileEdit.php');
+                        }
                     }
                 }
+
+
+
 
                 ?>
 
